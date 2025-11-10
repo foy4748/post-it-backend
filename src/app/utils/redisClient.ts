c084dbe3b139b07757873f90d61628f30df4da8b
foy4748 <faisaljfcl@gmail.com>
2025-11-10 18:20:37 +0600

    Configured bull to attempt 3 times

--- src/app/utils/redisClient.ts
+++ src/app/utils/redisClient.ts
@@ -1,5 +1,6 @@
 import { createClient } from 'redis';
 import config from '../config';
+import Bull from 'bull';
 
 export const redisClient = createClient({
   username: config.redisUser,
@@ -10,11 +11,18 @@ export const redisClient = createClient({
   },
 });
 
-export const redisForBull = {
+export const redisForBull: Bull.QueueOptions = {
   redis: {
     username: config.redisUser,
     password: config.redisPassword,
     host: config.redisHost,
     port: Number(config.redisPort),
+  },
+  defaultJobOptions: {
+    attempts: 3, // 2 retries + 1 initial attempt
+    backoff: {
+      type: 'exponential',
+      delay: 1000,
+    },
   },
 };
